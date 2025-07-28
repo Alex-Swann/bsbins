6d72f722c54a18996cf3a4be06c49113c9654cbc
Alex-Swann <alexander.swann@mhra.gov.uk>
2025-07-28 23:52:09 +0100

    Add privacy policy

---
+++ pages/privacy.js
@@ -0,0 +1,9 @@
+export default function PrivacyPolicy() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Privacy Policy</h1>
+      <p>This app does not collect, store, or share any personal information. It simply displays static content.</p>
+      <p>For questions, contact: <a href="mailto:alex.r.swann@gmail.com">alex.r.swann@gmail.com</a></p>
+    </main>
+  );
+}
